25efd41d34c1fa2721da841651fc8a04cd008a73
Radosław Malinowski <malinowski.rad@gmail.com>
2022-08-06 08:17:59 +0200

    add security login controller

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use ApiPlatform\Core\Api\IriConverterInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class SecurityController extends AbstractController
+{
+    public function login(IriConverterInterface $iriConverter)
+    {
+        if(!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->json([
+                'error' => 'Invalid login request: check that the Content-Type header is "application/json"'
+            ], 400);
+        }
+
+        return new Response(null, 204, ['Location' => $iriConverter->getIriFromItem($this->getUser())]);
+    }
+}
