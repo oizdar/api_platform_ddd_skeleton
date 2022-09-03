a7c84c13964176043f9e820d2e39cbeeace2ac1c
Radosław Malinowski <malinowski.rad@gmail.com>
2022-09-03 15:03:32 +0200

    role admin see all published & unpublished ExampleResourceEntities

--- src/ApiPlatform/ExampleResourceEntityIsPublishedExtension.php
+++ src/ApiPlatform/ExampleResourceEntityIsPublishedExtension.php
@@ -6,15 +6,23 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInter
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use App\Entity\ExampleResourceEntity;
 use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Security;
 
 class ExampleResourceEntityIsPublishedExtension implements QueryCollectionExtensionInterface
 {
+    public function __construct(private Security $security)
+    {
+    }
+
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null): void
     {
         if (ExampleResourceEntity::class !== $resourceClass) {
             return;
         }
 
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return;
+        }
         $rootAlias = $queryBuilder->getRootAliases()[0];
         $queryBuilder->andWhere(sprintf('%s.published = :published', $rootAlias))
             ->setParameter('published', true);
